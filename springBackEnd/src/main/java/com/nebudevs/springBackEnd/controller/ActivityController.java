dbe3ff6d7c848c6cd24ec12d9aff6dc9765531f6
MentseurFares <mentseur.fares@hotmail.fr>
2024-10-17 20:39:01 +0200

    authentifcation DONE!

--- springBackEnd/src/main/java/com/nebudevs/springBackEnd/controller/ActivityController.java
+++ springBackEnd/src/main/java/com/nebudevs/springBackEnd/controller/ActivityController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/activity")
+@RequestMapping("api/activity")
 public class ActivityController {
 
     @Autowired
