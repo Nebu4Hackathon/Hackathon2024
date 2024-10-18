d9e78b6c480fa98f5337db8393b910184d4ed0fa
MentseurFares <mentseur.fares@hotmail.fr>
2024-10-18 11:16:04 +0200

    test auth done!

--- springBackEnd/src/main/java/com/nebudevs/springBackEnd/security/WebSecurityConfig.java
+++ springBackEnd/src/main/java/com/nebudevs/springBackEnd/security/WebSecurityConfig.java
@@ -86,9 +86,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
             .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth ->
-                    auth.requestMatchers("/api/auth/**").permitAll()
-                            .requestMatchers("/api/test/**").permitAll()
-                            .requestMatchers("api/activity/**").permitAll()
+                    auth.requestMatchers("/api/auth/**", "api/activity/**" ).permitAll()
                             .anyRequest().authenticated()
             );
 
